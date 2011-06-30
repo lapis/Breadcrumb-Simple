69a6de55274cee5b7214e7d38aea9554bf38e703
Junichi Iwasaki <jiwasaki@idac.co.jp>
2011-06-30 13:00:20 +0900

    initial commit

---
+++ lib/Breadcrumb/Simple.pm
@@ -0,0 +1,32 @@
+package Breadcrumb::Simple;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Breadcrumb::Simple -
+
+=head1 SYNOPSIS
+
+  use Breadcrumb::Simple;
+
+=head1 DESCRIPTION
+
+Breadcrumb::Simple is
+
+=head1 AUTHOR
+
+jiwasaki E<lt>jiwasaki {at} idac.co.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
